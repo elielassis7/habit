f07ea5f2ac8b33db91fb24179e4d5039c08aa969
root <root@Assis-Matias-Oliveira>
2023-01-21 11:05:39 -0300

    setup

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import {PrismaClient} from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', async () => {
+   const habits = await prisma.habit.findMany({
+      where: {
+         title: {
+            startsWith: 'Beber'
+         }
+      }
+   })
+   
+   return habits
+})
+
+app.listen({ port: 3333,})
+.then(() => {console.log('Server run')})
---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import '.styles/global.css';
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3}/>
+      <Habit completed={10}/>
+      <Habit completed={33}/>
+      
+    </div>
+    )
+  
+}
+
+export default App
