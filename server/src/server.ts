100f20f7a945c54086e6e54b7df83cc76281bcf6
Eliel Assis <eliel.assis7@hotmail.com>
2023-01-25 07:14:51 -0300

    layout front-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import {PrismaClient} from '@prisma/client'
+import { appRoutes } from './routers'
+
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
-app.register(cors)
 
-app.get('/', async () => {
-   const habits = await prisma.habit.findMany({
-      where: {
-         title: {
-            startsWith: 'Beber'
-         }
-      }
-   })
-   
-   return habits
-})
+app.register(cors) 
+app.register(appRoutes)
+
+
 
 app.listen({ port: 3333,})
 .then(() => {console.log('Server run')})
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,21 @@
-import '.styles/global.css';
-import { Habit } from "./components/Habit"
+import './styles/global.css';
+//import { Habit } from "./components/Habit"
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3}/>
-      <Habit completed={10}/>
-      <Habit completed={33}/>
+    <div className='w-sreen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        
+        <Header/>
+        <SummaryTable/>
+
+      </div>
       
     </div>
     )
   
 }
 
-export default App
+
